2c1676e60c451f4166a6c49f26f7b862b6380bb5
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-05-28 09:27:23 +0530

    feat: added comment routes for calling comment controllers

---
+++ server/src/routes/commentRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+const router = express.Router();
+import { getComments, createComment, updateComment, deleteComment, reactToComment } from '../controllers/commentController..js';
+import { protect } from '../middleware/authMiddleware.js';
+
+router.use(protect);
+router.get('/', getComments);
+router.post('/', createComment);
+router.patch('/:id', updateComment);
+router.delete('/:id', deleteComment);
+router.post('/:id/react', reactToComment);
+
+export default router;
