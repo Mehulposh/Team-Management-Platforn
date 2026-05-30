7560876279af5681980714e12662368d996bdc08
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-05-30 16:48:30 +0530

    fix: fixed the import statements

--- client/src/api/apiInstance.js
+++ client/src/api/apiInstance.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useAuthStore } from '../store/authStore';
+import { useAuthStore } from '../zustand/authStore.js';
 
 const api = axios.create({
   baseURL: '/api',
